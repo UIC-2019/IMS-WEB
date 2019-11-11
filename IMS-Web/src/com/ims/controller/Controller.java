508a46640ef6e32fc570f49938f8eca745abbb7d
harshitha719 <55902125+harshitha719@users.noreply.github.com>
2019-11-10 20:01:18 -0600

    Add files via upload

--- IMS-Web/src/com/ims/controller/Controller.java
+++ IMS-Web/src/com/ims/controller/Controller.java
@@ -3,19 +3,46 @@
  */
 package com.ims.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.ims.impl.DisplayInventoryImpl;
+import com.ims.impl.EmailMenuImpl;
+import com.ims.utils.InventoryBean;
+
 /**
  * @author Vartika 
  * 		   This class is an interface between jsp and actual logic
  *         implementation classes. It will have methods to call implementation
  *         classes for subsequent logic operations.
  */
-public class Controller {
 
+public class Controller {
+public ArrayList<InventoryBean> inventoryList = new ArrayList<InventoryBean>();
+public String senderEmail;
+public DisplayInventoryImpl display = new DisplayInventoryImpl();
+public EmailMenuImpl mail = new EmailMenuImpl();
 	/**
 	 * 
 	 */
 	public Controller() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public ArrayList<InventoryBean>showInventory(){
+		inventoryList = display.loadInventory();
+			return inventoryList;
+	}
+	
+	public void sendMail(){
+	  mail.sendEmail(senderEmail);
 	}
 
+	public String getSenderEmail() {
+		return senderEmail;
+	}
+
+	public void setSenderEmail(String senderEmail) {
+		this.senderEmail = senderEmail;
+	}
 }
